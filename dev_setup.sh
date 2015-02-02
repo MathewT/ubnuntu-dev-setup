b7aa5f849019367eeee3d602d55c227b1f987b5d
MathewT <mathew.thomas@longdivision.com>
2015-02-02 10:25:09 -0800

    Initial commit.

---
+++ dev_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "setting up a Ruby/Golang/Java/Docker dev environment for Ubuntu."
+
+sudo apt-get install -y build-essential vim git-core curl libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties mysql-client libmysqlclient-dev zlib1g-dev libpcre3 libpcre3-dev libgdbm-dev libncurses5-dev automake libtool bison libffi-dev python-dev zlib1g-dev software-properties-common
+
+## Install rvm to manage Ruby installs
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+curl -L https://get.rvm.io | bash 
+source ~/.rvm/scripts/rvm
+echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
+
