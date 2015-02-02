cd134b1048da44cbb015ad3591b67b9f315cea7f
MathewT <mathew.thomas@longdivision.com>
2015-02-02 10:37:30 -0800

    Initial commit.

--- dev_setup.sh
+++ dev_setup.sh
@@ -4,7 +4,7 @@ echo "setting up a Ruby/Golang/Java/Docker dev environment for Ubuntu."
 
 cd $HOME
 echo $PWD
-sudo apt-get install -y build-essential vim git-core curl libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties mysql-client libmysqlclient-dev zlib1g-dev libpcre3 libpcre3-dev libgdbm-dev libncurses5-dev automake libtool bison libffi-dev python-dev zlib1g-dev software-properties-common
+sudo apt-get install -y build-essential vim git-core curl libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties mysql-client libmysqlclient-dev zlib1g-dev libpcre3 libpcre3-dev libgdbm-dev libncurses5-dev automake libtool bison libffi-dev python-dev zlib1g-dev software-properties-common ctags
 
 ## Install rvm to manage Ruby installs
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
@@ -12,8 +12,5 @@ curl -L https://get.rvm.io | bash
 source ~/.rvm/scripts/rvm
 echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
 
-wget http://amix.dk/vim/vimrc.txt
-mv vimrc.txt .vimrc
-
-#TODO Edit .vimrc and set the tabwidth to 2
-
+git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
